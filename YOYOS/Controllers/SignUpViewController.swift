5791b233ad4dcd944c106e03fafc5ac999be91f3
Mr Ocumare <ilyaocumare@gmail.com>
2019-04-29 15:12:43 +0300

    strat work with firebase

---
+++ YOYOS/Controllers/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  YOYOS
+//
+//  Created by Mr.Ocumare on 29/04/2019.
+//  Copyright © 2019 park.mail.ru. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
